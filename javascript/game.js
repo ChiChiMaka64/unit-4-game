f08d087c15d3912884f62f7c7a2a41d7edd29254
Lynette Okoroike <lcokoroike@gmail.com>
2018-06-30 14:10:52 -0700

    WIP.

--- javascript/game.js
+++ javascript/game.js
@@ -1,51 +1,71 @@
-var crystal1 ="images/pink-crystal.jpg";
-var crystal2 ="images/blue-crystal.jpg";
-var crystal3 ="images/purple-crystal.jpg";
-var crystal4 ="images/gold-crystal.jpg";
-var wins=0;
-var losses=0;
-var score=0;
+var crystal1 = "images/pink-crystal.jpg";
+var crystal2 = "images/blue-crystal.jpg";
+var crystal3 = "images/purple-crystal.jpg";
+var crystal4 = "images/gold-crystal.jpg";
+var wins = 0;
+var losses = 0;
+var score = 0;
 
-var numberStart=19;
-var numberEnd=120;
-var number=[];
-while (numberStart <= numberEnd -1){
-    number.push(numberStart++);
-}
+var numberStart = 19;
+var numberEnd = 120;
+var number = [];
+//while (numberStart <= numberEnd - 1) {
+   // number.push(numberStart++);
+//}
 
 
 
-var RandomNumber= getRandomInt(numberStart, numberEnd);
+var RandomNumber = getRandomInt(numberStart, numberEnd);
+$("#Random-Number").text(RandomNumber)
 var crystal1Number = getRandomInt(1, 12);
 var crystal2Number = getRandomInt(1, 12);
 var crystal3Number = getRandomInt(1, 12);
 var crystal4Number = getRandomInt(1, 12);
 
-$("#crystal1").on("click", function() {
-  console.log($(this));
-  score=score+crystal1Number;
+$("#crystal1").on("click", function () {
+    console.log($(this));
+    score = score + crystal1Number;
+   $("#Total-Score").text(score)
+   ProgressChecker()
 });
 
-$("#crystal2").on("click", function() {
-    console.log($(this));
-    score=score+crystal2Number;
-  });
- 
-  $("#crystal3").on("click", function() {
-    console.log($(this));
-    score=score+crystal3Number;
-  });
 
-  $("#crystal4").on("click", function() {
+$("#crystal2").on("click", function () {
     console.log($(this));
-    score=score+crystal4Number;
-  });
+    score = score + crystal2Number;
+    $("#Total-Score").text(score)
+    ProgressChecker()
+});
+
+$("#crystal3").on("click", function () {
+    console.log($(this));
+    score = score + crystal3Number;
+    $("#Total-Score").text(score)
+    ProgressChecker()
+});
+
+$("#crystal4").on("click", function () {
+    console.log($(this));
+    score = score + crystal4Number;
+    $("#Total-Score").text(score)
+    ProgressChecker()
+});
+
+function ProgressChecker() {
+    if (RandomNumber===score) {
+        alert("You Won!")
+    }
+    if (score > RandomNumber) {
+        alert("You Lose. Try Again.")
+    }
+}
 
 
- /**
- * Returns a random integer between min (inclusive) and max (inclusive)
- * Using Math.round() will give you a non-uniform distribution!
- */
+
+/**
+* Returns a random integer between min (inclusive) and max (inclusive)
+* Using Math.round() will give you a non-uniform distribution!
+*/
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min )) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
